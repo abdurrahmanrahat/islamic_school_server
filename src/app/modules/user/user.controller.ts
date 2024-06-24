2c582d241b7ac92f5ee12362bacd28e260d40c1c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-06-24 22:05:59 +0600

    islamic school server initialize

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import { sendResponse } from '../../utils/sendResponse';
+import { UserServices } from './user.service';
+
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const user = req.body;
+
+  const result = await UserServices.createUserInfoDb(user);
+
+  res.status(201).json({
+    success: true,
+    message: 'User registered successfully',
+    data: result,
+  });
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User register successfully',
+    data: result,
+  });
+});
+
+export const UserControllers = {
+  createUser,
+};
